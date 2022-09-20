ce9deb6d29c4daae10ed42934643baca1974455e
Yovangga Anandhika <dhikaprivate@gmail.com>
2022-09-20 18:53:59 +0800

    revision code

--- test/index.ts
+++ test/index.ts
@@ -3,7 +3,7 @@ import { Server, Options } from "./../src";
 (async () => {
     await Server({
         state : Options.Server.State.SERVER_STATE_DEVELOPMENT,
-        engine : Options.Server.Engine.FASTIFY
+        engine : Options.Server.Engine.FASTIFY,
     }).then(async (res) => {
         console.log(JSON.stringify(res));
     }).catch(async (error) => {
